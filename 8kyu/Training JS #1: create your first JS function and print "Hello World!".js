c0ac9d880ccae027ade7043b3bf5267d6d42a788
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2022-12-27 23:51:33 +0300

    Update Training JS #1: create your first JS function and print "Hello World!".js

--- 8kyu/Training JS #1: create your first JS function and print "Hello World!".js
+++ 8kyu/Training JS #1: create your first JS function and print "Hello World!".js
@@ -1,6 +1,6 @@
 function helloWorld() {
   // creates variable called str to str that equals Hello World
-  var str = "Hello World!";
+ const str = "Hello World!";
   // prints the variable str to the console
   console.log(str);
 };
