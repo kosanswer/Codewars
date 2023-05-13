235b06e8ed3660a4439f475994cd723c01f527bc
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-05-13 09:50:45 +0300

    Update Convert a Boolean to a String.js

--- 8kyu/Convert a Boolean to a String.js
+++ 8kyu/Convert a Boolean to a String.js
@@ -6,3 +6,15 @@ function booleanToString(b){
    return 'false'
  }
 }
+// second solution
+function booleanToString(b){
+  return b.toString();
+}
+// third solution
+function booleanToString(b){
+  return b ? 'true' : 'false';
+}
+// or 
+function booleanToString(b){
+  return String(b);
+}
