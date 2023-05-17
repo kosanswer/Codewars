8eb1c2873ddfd6b2d319d7d7de142a16ce60a7ce
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-05-17 18:38:31 +0300

    Update Will you make it?.js

--- 8kyu/Will you make it?.js
+++ 8kyu/Will you make it?.js
@@ -1,3 +1,5 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   return mpg * fuelLeft >= distanceToPump
 };
+// or 
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
