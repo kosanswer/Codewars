d0cac34501968c22eb810dfbb64ae4988dd54a60
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-05-19 21:29:45 +0300

    Create Brake camelCase.js

---
+++ 6kyu/Brake camelCase.js
@@ -0,0 +1,16 @@
+/*Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""*/
+////////////////////////////////////////////
+function solution(string) {
+    let str = string.split('')
+    for(let i=0; i < str.length; i++){
+        if(str[i] >= 'A' && str[i] <= 'Z'){       
+            str[i] = " " +str[i]
+        }   
+    }
+        return str.join('')
+}
