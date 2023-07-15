2fc5fb7cd8d988244e815a7a6e0f2e9a66a41572
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-07-15 14:03:13 +0300

    Update Keep up the hoop.js

--- 8kyu/Keep up the hoop.js
+++ 8kyu/Keep up the hoop.js
@@ -12,3 +12,5 @@ function hoopCount(n) {
     return "Keep at it until you get it";
   }
 }
+////////////////////////////////////or
+const hoopCount = n => n < 10 ? 'Keep at it until you get it' : 'Great, now move on to tricks';
