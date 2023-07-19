0d10caef16a529608b75cab9f79e7fcc0fbb6588
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-07-19 17:54:10 +0300

    Update Drink about.js

--- 8kyu/Drink about.js
+++ 8kyu/Drink about.js
@@ -33,6 +33,12 @@ function peopleWithAgeDrink(age) {
   return "drink whisky";
   }
 }
+  ////////////////////////////////////
+  const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
+
 
 
   
