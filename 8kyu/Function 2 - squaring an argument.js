8a3b31091197a0bee12e2cd5f08a3e5c64f48672
Konstantin Gordienko <98546853+kosanswer@users.noreply.github.com>
2023-07-29 20:15:38 +0300

    Create Function 2 - squaring an argument.js

---
+++ 8kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,11 @@
+/*
+Now you have to write a function that takes an argument and returns the square of it.
+
+
+*/
+function square(num) {
+  return num * num;
+}
+
+// Test cases
+console.log(square(5)); // Output: 25
